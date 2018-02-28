dfba156d9698f516efffb3fafaca93f8867e8151
Thomas Seaman <thomasseaman@Thomas-School-MacBook-Pro.local>
2018-02-28 12:45:31 -0800

    push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,31 @@
 $(document).ready(function () {
-
+  
   var gifArray = ["surfing", "gym", "basketball", "swing", "classroom", "zoo", "dog", "cooking", "santa", "olympic"]
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5YG1izQ996GdM5LUGUHgZgCNfF9L8Dmc&q=" + gifArray + "%20fails" + "&limit=10&offset=0&rating=g&lang=en";
 
-  var searchGiphy = function () {
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5YG1izQ996GdM5LUGUHgZgCNfF9L8Dmc&q=" + gifArray + "%20fails" + "&limit=10&offset=0&rating=g&lang=en";
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-    })
-      .then(function (resp) {
-        console.log(resp);
-      })
-      .catch(function (err) {
-        throw err;
-        console.log(err);
-      });
-  }
 
   for (var i = 0; i < gifArray.length; i++) {
     $('<button type="submit" class="btn btn-primary" id="gif-button' + [i] + '">' + gifArray[i] + " fails" + '</button>').appendTo('#button-header');
   }
 
-
   $('#search-button').on('click', function () {
     var userSearch = $('#user-input').val();
     gifArray.push(userSearch);
-    $('<button type="submit" class="btn btn-primary" id="search-button">' + userSearch + " fails" + '</button>').appendTo('#button-header');
+    $('<button type="submit" class="btn btn-primary" id="gif-button' + [i] + '">' + userSearch + " fails" + '</button>').appendTo('#button-header');
   })
 
-  console.log(searchGiphy());
-  
+
   for (var i = 0; i < gifArray.length; i++) {
-    $("#gif-button"+[i]).on("click", function () {
-    console.log(resp.data)
-    })
+    $("#gif-button" + [i]).on("click", function () {
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (response) {
+        console.log(response)
+      }).catch(function (err) {
+          // console.error(err);
+        })
+      })
   }
 
 
